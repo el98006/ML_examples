41dbfdd77148dc27be3eb14d068454d21d3fc152
el98006 <el98006@gmail.com>
2017-05-05 16:20:14 -0700

    add model evaluation and more classifiers

--- movie_review.py
+++ movie_review.py
@@ -12,16 +12,22 @@ from sklearn.datasets import load_files
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import classification_report, f1_score, precision_score, recall_score, roc_auc_score
 from sklearn.pipeline import Pipeline
+from sklearn.grid_search import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+import time
+from sklearn.ensemble.forest import RandomForestClassifier
 
 vectorizer_list = [CountVectorizer(), TfidfVectorizer(min_df=5, max_df=0.8, sublinear_tf=True, use_idf=True)]
+vectorizer_names = ['CountVectorizer', 'TfidfVectorizer']
 #traditional term count, tfidf
 #tfidf sublinear_tf=True to use logarithmic TF.
 
-classifier_list = [SVC(), SVC(kernel='linear'), MultinomialNB()]
-classifier_names = ['SVC', 'SVC linear kernel', 'MultinomialNB',]
-def evaluation(prediction, labels):
-    
-    return True
+classifier_list = [SVC(), SVC(kernel='linear'), MultinomialNB(), RandomForestClassifier(n_estimators = 100)]
+classifier_names = ['SVC', 'SVC linear kernel', 'MultinomialNB', 'RandomForest']
+
+VERBOSE_MODE = False
+
+
 
 
 def load_data():
@@ -32,7 +38,10 @@ def load_data():
     cur_dir = os.path.dirname(__file__)
     data_dir = os.path.join(cur_dir, 'movie_data/txt_sentoken/')
     data = load_files(data_dir)
-    return train_test_split(data.data, data.target, test_size=0.2, random_state=96)    
+    train_data, test_data, train_labels, test_labels = train_test_split(data.data, data.target, test_size=0.2, random_state=96)
+  
+    return train_data, test_data, train_labels, test_labels  
+        
 
 def feature_extraction(train_data, test_data, vectorizer):
     '''Note
@@ -54,16 +63,16 @@ def feature_extraction(train_data, test_data, vectorizer):
     print 'num of docs, num of features in trainset : {}'.format(train_features.shape)
     print 'shape of test_features {}'.format(test_features.shape)
     
-    '''
-    i = 450
-    j = 10
-    words = vectorizer.get_feature_names()[i:i+10]
-    chunk = pandas.DataFrame(test_features[j:j+7,i:i+10].todense(), columns=words)
-    print 'peek into test_features: {}'.format(chunk)
-    '''
+    if VERBOSE_MODE is True:
+        i = 450
+        j = 10
+        words = vectorizer.get_feature_names()[i:i+10]
+        chunk = pandas.DataFrame(test_features[j:j+7,i:i+10].todense(), columns=words)
+        print 'peek into test_features: {}'.format(chunk)
+    
     return train_features, test_features
 
-def classify(train_features, train_labels,test_features, test_labels, classifier):
+def classify(train_features, train_labels,test_features, classifier):
     
     '''
     Note:
@@ -78,20 +87,54 @@ def evaluate_models():
     '''Note
     F1 score 2*((precision*recall)/(precision+recall)), F1 score conveys the balance between the precision and the recall
     '''
+    scores={}
+    
     train_data, test_data, train_labels, test_labels = load_data()
-    for classifier in classifier_list:
-        for vectorizer in vectorizer_list:    
-            print 'vectorizer:{}, classifier:{}'.format(vectorizer.__class__.__name__, classifier_names[classifier_list.index(classifier)]) 
+  
+    for ci, classifier in enumerate(classifier_list):
+        for vi, vectorizer in enumerate(vectorizer_list):    
+            print 'vectorizer:{}, classifier:{}'.format(vectorizer.__class__.__name__, classifier_names[classifier_list.index(classifier)])
+            t_start = time.time() 
             train_features, test_features = feature_extraction(train_data, test_data, vectorizer)
-            prediction = classify(train_features, train_labels, test_features, test_labels, classifier)
+            prediction = classify(train_features, train_labels, test_features,  classifier)
+            time_spent = time.time() - t_start
+            scores[f1_score(test_labels, prediction)] = (ci, vi, time_spent)
             print classification_report(test_labels, prediction)
+    sorted_scores = sorted(scores, reverse =True)
+    ranking = [ (scores[r], r) for r in sorted_scores]
+    print '{:<20} {:<20} {:<5} {:<5}'.format('classifier', 'Vectorizer', 'F1', 'Time')
+    for (ci, vi, time_spent), f1 in ranking:
+        print '{:<20} {:<20} {:<5.2f} {:>5.2f}s'.format(classifier_names[ci], vectorizer_names[vi], f1, time_spent)
                 
 def evaluate_models_with_pipeline():
-    train_data, test_data, train_labels, test_labels = load_data()
-    model = Pipeline([('vectorizer', vectorizer_list), ('classifier', classifier_list)])
-    model.fit(train_data, test_data, train_labels, test_labels)
-    return model
+    '''Note
+    Pipeline two interfaces: transformer and estimator.
+    GridSearchCV performs an exhaustive search on specified parameter values. 
+    This allows iteration through defined sets of parameters and the reporting of the result in the form of various metrics.
+     
+    '''
+  
+    steps = [('tfidf', TfidfVectorizer()), ('classifier', SVC())]
+    '''
+    params = [ 'tfidf':{'min_df':[5, 10, 20], 'max_df':[0.8, 0.9, 0.95]}, 
+              'classifier': {'kernel':['rbf', 'sublinear']}
+             ]
+    '''
+    scores = ['precision', 'recall']
+    train_data, test_data, train_target, test_target = load_data()
+    pipeline = Pipeline(steps)
+    '''
+    pipeline.fit(train_data, train_target)
+    prediction = pipeline.predict(test_data)
+    report = classification_report(test_target, prediction)
+    print report
+    '''
+    clf = GridSearchCV(pipeline, cv=3, n_jobs=2, param_grid=params)
+    clf.fit(train_data, train_target)
+    mean_scores = np.array(clf.cv_results_['mean_test_scores'])
+    print mean_scores
+    
         
 if __name__ == '__main__':
     evaluate_models()
-    
+    #evaluate_models_with_pipeline()
